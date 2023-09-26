0b2f9e009c7e73548879445a544da4961f47c360
Stefano Falda <stefano.falda@gmail.com>
2023-09-26 23:03:06 +0200

    Test removed

--- test/photo_organizer_test.dart
+++ test/photo_organizer_test.dart
@@ -1,8 +1,1 @@
-import 'package:photo_organizer/photo_organizer.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
+void main() {}
